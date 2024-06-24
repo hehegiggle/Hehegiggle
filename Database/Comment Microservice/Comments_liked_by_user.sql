c6b507944d13ea01754bfba5c40f5175e9f98c30
hehegiggle <hehegiggleconnect@gmail.com>
2024-06-24 23:15:55 +0530

    Add files via upload

---
+++ Database/Comment Microservice/Comments_liked_by_user.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `comments_liked_by_users` (
+  `comments_comment_id` int NOT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `id` int DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `userimage` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
+  KEY `FK2r9p2rc5x46alsu7u6g3fi8cg` (`comments_comment_id`),
+  CONSTRAINT `FK2r9p2rc5x46alsu7u6g3fi8cg` FOREIGN KEY (`comments_comment_id`) REFERENCES `comments` (`comment_id`)
+) 
---
+++ Database/Reel Microservice/Reel.sql
@@ -0,0 +1,11 @@
+CREATE TABLE `reel` (
+  `id` int NOT NULL,
+  `caption` varchar(255) DEFAULT NULL,
+  `user_email` varchar(255) DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `user_image` varchar(255) DEFAULT NULL,
+  `user_username` varchar(255) DEFAULT NULL,
+  `video` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) 
