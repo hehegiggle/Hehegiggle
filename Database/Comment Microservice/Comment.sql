dec8680fe2e0784e8f0096d62a5330b2331acfb6
Chetan Almaje <145182964+Chetan1681@users.noreply.github.com>
2024-07-23 00:07:45 +0530

    Update Comment.sql

--- Database/Comment Microservice/Comment.sql
+++ Database/Comment Microservice/Comment.sql
@@ -6,10 +6,12 @@ CREATE TABLE `comments` (
   `postid` varchar(255) DEFAULT NULL,
   `image` varchar(255) DEFAULT NULL,
   `location` varchar(255) DEFAULT NULL,
-  `user_email` varchar(255) DEFAULT NULL,
   `user_id` int DEFAULT NULL,
+  `user_username` varchar(255) DEFAULT NULL,
+  `user_email` varchar(255) DEFAULT NULL,
+  `user_idd` int DEFAULT NULL,
   `user_name` varchar(255) DEFAULT NULL,
   `user_image` varchar(255) DEFAULT NULL,
-  `user_username` varchar(255) DEFAULT NULL,
+  `user_usernamee` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`comment_id`)
-) 
+)
