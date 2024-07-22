81995973fb8a2180f45978102e955d6d2bd5a87d
Chetan Almaje <145182964+Chetan1681@users.noreply.github.com>
2024-07-23 00:12:40 +0530

    Update Chat.sql

--- Database/Chat Microservice/Chat.sql
+++ Database/Chat Microservice/Chat.sql
@@ -1,7 +1,5 @@
 CREATE TABLE `chat` (
   `id` int NOT NULL,
-  `chat_image` varchar(255) DEFAULT NULL,
-  `chat_name` varchar(255) DEFAULT NULL,
   `timestamp` datetime(6) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) 
+)
