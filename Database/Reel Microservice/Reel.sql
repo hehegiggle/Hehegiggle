01f56c89b84b6b3ad0c5f363cd8d8964700e698e
Chetan Almaje <145182964+Chetan1681@users.noreply.github.com>
2024-07-22 22:26:27 +0530

    Update Reel.sql

--- Database/Reel Microservice/Reel.sql
+++ Database/Reel Microservice/Reel.sql
@@ -1,11 +1,12 @@
 CREATE TABLE `reel` (
   `id` int NOT NULL,
   `caption` varchar(255) DEFAULT NULL,
+  `timestamp` datetime(6) DEFAULT NULL,
   `user_email` varchar(255) DEFAULT NULL,
   `user_id` int DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
   `user_image` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
   `user_username` varchar(255) DEFAULT NULL,
   `video` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) 
+)
