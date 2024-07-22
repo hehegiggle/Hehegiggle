459d725d6c1d83fe01dd71ad6d3bc30701c5caa8
Chetan Almaje <145182964+Chetan1681@users.noreply.github.com>
2024-07-23 00:13:16 +0530

    Update Chat_users.sql

--- Database/Chat Microservice/Chat_users.sql
+++ Database/Chat Microservice/Chat_users.sql
@@ -5,8 +5,10 @@ CREATE TABLE `chat_users` (
   `followings` varbinary(255) DEFAULT NULL,
   `gender` varchar(255) DEFAULT NULL,
   `id` int DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
   `name` varchar(255) DEFAULT NULL,
   `password` varchar(255) DEFAULT NULL,
+  `username` varchar(255) DEFAULT NULL,
   KEY `FKglok2i2m8cbulbt5xxmfqixw3` (`chat_id`),
   CONSTRAINT `FKglok2i2m8cbulbt5xxmfqixw3` FOREIGN KEY (`chat_id`) REFERENCES `chat` (`id`)
-) 
+)
